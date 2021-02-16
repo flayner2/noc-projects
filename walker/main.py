e97db80a57b776577358de9074308bb0914149f3
flayner2 <flayner5@gmail.com>
2021-02-16 20:53:58 -0300

    Made the Walker change colors dynamically

--- walker/main.py
+++ walker/main.py
@@ -2,13 +2,25 @@ import pygame
 import walker
 import sys
 import time
+from random import randint
 
 
 # Constants
 # The window size
-W_WIDTH, W_HEIGHT = (640, 480)
-# The window color == white
-W_COLOR = pygame.Color(255, 255, 255)
+W_WIDTH, W_HEIGHT = (800, 600)
+# Colors
+COLORS = {
+    "WHITE": pygame.Color(255, 255, 255),
+    "BLACK": pygame.Color(0, 0, 0),
+    "RED": pygame.Color(255, 0, 0),
+    "GREEN": pygame.Color(0, 255, 0),
+    "BLUE": pygame.Color(0, 0, 255),
+    "YELLOW": pygame.Color(255, 255, 0),
+    "PURPLE": pygame.Color(127, 0, 255),
+    "ORANGE": pygame.Color(204, 102, 0),
+}
+# Window color
+W_COLOR = COLORS["WHITE"]
 
 
 def main() -> None:
@@ -24,7 +36,11 @@ def main() -> None:
     # Walker position in (x, y)
     xpos, ypos = (W_WIDTH / 2, W_HEIGHT / 2)
     # Walker size in (width, height)
-    width, height = (2, 2)
+    width, height = (5, 5)
+    # Wakler color
+    curr_color = COLORS["BLACK"]
+    # Count the number of Walker steps
+    steps = 0
 
     # Initialize a Walker
     agent = walker.Walker(xpos, ypos, width, height)
@@ -41,17 +57,33 @@ def main() -> None:
                     pygame.quit()
                     sys.exit()
 
-        # Draw the Walker to the screen
-        agent.draw(screen)
+        # Change the color depending on where the Walker is
+        # if agent.left < (W_WIDTH / 2) and agent.top < (W_HEIGHT / 2):
+        #     curr_color = COLORS["RED"]
+        # elif agent.left < (W_WIDTH / 2) and agent.top > (W_HEIGHT / 2):
+        #     curr_color = COLORS["BLUE"]
+        # elif agent.left > (W_WIDTH / 2) and agent.top < (W_HEIGHT / 2):
+        #     curr_color = COLORS["ORANGE"]
+        # else:
+        #     curr_color = COLORS["PURPLE"]
+
+        # Change the color depending on the number of steps
+        if steps % 1000 == 0:
+            curr_color = (randint(0, 255), randint(0, 255), randint(0, 255))
+
+        agent.draw(screen, curr_color)
 
         # Make the Walker... walk
         agent.step()
 
         # Slow the draw loop down
-        time.sleep(0.05)
+        # time.sleep(0.005)
 
         # Draw everything to the screen
         pygame.display.flip()
+
+        # Increase the number of steps
+        steps += 1
 
 
 if __name__ == "__main__":
