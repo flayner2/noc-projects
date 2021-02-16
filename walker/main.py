5f94fa5b3c2af8b422491c8dfff5cdb088058dad
flayner2 <flayner5@gmail.com>
2021-02-16 17:02:13 -0300

    Updated the state of the branch

---
+++ walker/main.py
@@ -0,0 +1,9 @@
+import ursina
+import walker
+
+
+app = ursina.Ursina()
+
+agent = walker.Walker(0, 0)
+
+app.run()
---
+++ walker/walker.py
@@ -0,0 +1,51 @@
+import ursina
+from random import randint
+
+
+class Walker(ursina.Entity):
+    """The Walker class is a simple random walker agent represented by a
+    two-dimensional square
+    """
+
+    def __init__(self, x: float, y: float, color: ursina.Color = ursina.color.white):
+        """Instantiates a Walker object at position (x, y, 0) with color = `color`,
+        which defaults to white
+
+        Args:
+            x (float): position on the x-axis
+            y (float): position on the y-axis
+            color (ursina.Color, optional): the color for the Walker object. Defaults
+            to ursina.color.white.
+        """
+        super().__init__()
+
+        # Model is hard-coded to be a 2D square, but can be changed with Walker.model
+        self.model = "quad"
+
+        # Walker's intial position on the screen
+        self.x = x
+        self.y = y
+
+        # Walker's color, defaults to white
+        self.color = color
+
+    def step(self, step_size: float = 0.01):
+        """Updates the Walker's position based on a step size
+
+        Args:
+            step_size (float, optional): the size of each step. Defaults to 0.01.
+        """
+        # Time delta to make animations look smooth
+        delta = ursina.time.dt
+        # The actual step the Walker is going to take each frame
+        offset = step_size * delta
+        choice = randint(1, 4)
+
+        if choice == 1:
+            self.x += offset
+        elif choice == 2:
+            self.x -= offset
+        elif choice == 3:
+            self.y += offset
+        else:
+            self.y -= offset
