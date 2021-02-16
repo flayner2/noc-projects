0bd028bdcbdfbc68e3b36d77c03821bd54093dd3
flayner2 <flayner5@gmail.com>
2021-02-16 19:09:32 -0300

    Now using pygame instead of ursina

--- walker/walker.py
+++ walker/walker.py
@@ -1,62 +1,43 @@
-import ursina
+import pygame
 from random import uniform
 
 
-class Walker(ursina.Entity):
+class Walker(pygame.Rect):
     """The Walker class is a simple random walker agent represented by a
-    two-dimensional square
+    two-dimensional square.
     """
 
     def __init__(
         self,
-        x: float,
-        y: float,
-        color: ursina.Color = ursina.color.white,
-        scale: tuple[float] = (0.05, 0.05),
+        x: int,
+        y: int,
+        w: int,
+        h: int,
     ):
-        """Instantiates a Walker object at position (x, y, 0) with color = `color`,
-        which defaults to white
+        """Instantiates a Walker object at position `(x, y)` and with width and
+        height = `(w, h)`.
 
         Args:
-            x (float): position on the x-axis
-            y (float): position on the y-axis
-            color (ursina.Color, optional): the color for the Walker object. Defaults
-            to ursina.color.white.
-            scale (tuple[float]): x and y scale of the Walker object
+            x (int): the position of the Walker object in the x axis
+            y (int): the position of the Walker object in the x axis
+            w (int): the width of the Walker object
+            h (int): the height of the Walker object
         """
         super().__init__()
 
-        # Model is hard-coded to be a 2D square, but can be changed with Walker.model
-        self.model = "quad"
-
         # Walker's intial position on the screen
-        self.x = x
-        self.y = y
+        self.left = x
+        self.top = y
 
         # The size of the Walker
-        self.scale = scale
-
-        # Walker's color, defaults to white
-        self.color = color
-
-        self.eternal = True
-        self.always_on_top = True
+        self.width = w
+        self.height = h
 
     def step(self):
-        """Updates the Walker's position based on a step size
+        """Updates the Walker's position"""
+        # The value for each axis of the step the Walker is going to take each frame
+        x_step = uniform(-1, 1)
+        y_step = uniform(-1, 1)
 
-        Args:
-            step_size (float, optional): the size of each step. Defaults to 0.01.
-        """
-        # Time delta to make animations look smooth
-        delta = ursina.time.dt
-        # The actual step the Walker is going to take each frame
-        x_step = uniform(-1, 1) * delta
-        y_step = uniform(-1, 1) * delta
-
-        self.x += x_step
-        self.y += y_step
-
-    def update(self):
-        """Updates the Walker's position each frame"""
-        self.step()
+        self.left += x_step
+        self.top += y_step
