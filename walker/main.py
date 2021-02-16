c79e4aadae0a38880968ca84e62110f3fcb26278
flayner2 <flayner5@gmail.com>
2021-02-16 19:29:04 -0300

    Switched the main script to conform to pygame

--- walker/main.py
+++ walker/main.py
@@ -1,10 +1,31 @@
-import ursina
+import pygame
 import walker
+import sys
 
-app = ursina.Ursina()
 
-app.win.setClearColorActive(False)
+# Constants
+# The resolution variable, used to calculate each object's size
+RES = 10
+# The window size
+W_WIDTH, W_HEIGHT = (640, 480)
+# The window color == white
+W_COLOR = pygame.Color(255, 255, 255)
 
-agent = walker.Walker(0, 0, scale=(0.02, 0.02))
 
-app.run()
+def main() -> None:
+    # Initializing pygame's runtime
+    pygame.init()
+
+    # Initializing the screen
+    screen = pygame.display.set_mode((W_WIDTH, W_HEIGHT))
+
+    # Draw loop
+    while True:
+        for event in pygame.event.get():
+            # Catch a QUIT event to terminate the loop and the app
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+
+if __name__ == "__main__":
+    main()
