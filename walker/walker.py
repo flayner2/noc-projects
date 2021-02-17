706ff903af922be4c2d2ed3f133a5b121da6f170
flayner2 <flayner5@gmail.com>
2021-02-16 21:28:27 -0300

    Created a mouse-following Walker

--- walker/walker.py
+++ walker/walker.py
@@ -83,3 +83,57 @@ class BottomRightSkewedWalker(Walker):
             self.top += 1
         else:
             self.top -= 1
+
+
+class MouseFollowerWalker(Walker):
+    """A Walker class with a tendency to move towards the cursor position"""
+
+    def __init__(self, x: int, y: int, w: int, h: int) -> None:
+        """Instantiates a BottomRightSkewedWalker object at position `(x, y)` and with width and
+        height = `(w, h)`.
+
+        Args:
+            x (int): the position of the Walker object in the x axis
+            y (int): the position of the Walker object in the x axis
+            w (int): the width of the Walker object
+            h (int): the height of the Walker object
+        """
+        super().__init__(x, y, w, h)
+
+    def step(self, probability: float = 0.5) -> None:
+        """Updates the Walker's position, with a bottom-right skew
+
+        Args:
+            probability (float, optional): The probability that the walker will try to
+            move towards the cursor. Defaults to 0.5.
+        """
+        # The chance of the Walker moving towards the cursor
+        chance = uniform(0, 1)
+
+        # The value for each axis of the step the Walker is going to take each frame
+        # if chance > probability
+        x_step = randint(-1, 1)
+        y_step = randint(-1, 1)
+
+        # If the Walker feels like following the mouse...
+        if chance <= probability:
+            # Get the current position of the cursor
+            mouse_pos = pygame.mouse.get_pos()
+
+            # If the cursor is further right, move right
+            if mouse_pos[0] - self.left > 0:
+                self.left += 1
+            # Else, move left
+            else:
+                self.left -= 1
+
+            # If the cursor is further down, move down
+            if mouse_pos[1] - self.top > 0:
+                self.top += 1
+            # Else, move up
+            else:
+                self.top -= 1
+        # Otherwise, just move randomly
+        else:
+            self.left += x_step
+            self.top += y_step
