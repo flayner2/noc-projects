29fbca8899714f8e188170ec5936c121cbad96ff
flayner2 <flayner5@gmail.com>
2021-02-23 21:22:33 -0300

    Created a visualization for a walker with noise influencing its step size

--- walker/main.py
+++ walker/main.py
@@ -26,7 +26,10 @@ x, y = W_WIDTH / 2, W_HEIGHT / 2
 # agent = walker.ExponentialMonteCarloWalker(
 #     x, y, stroke=Colors.black, min=-1, max=1, exp=2
 # )
-agent = walker.NoiseWalker(x, y, stroke=Colors.black, tx=0, ty=1000, inc=0.001)
+# agent = walker.NoiseWalker(x, y, stroke=Colors.black, tx=0, ty=1000, inc=0.001)
+agent = walker.NoiseWalker(
+    x, y, stroke=Colors.black, tx=0, ty=1000, inc=0.001, stepsize=True
+)
 
 
 # Setup function, runs once before the draw loop
@@ -37,6 +40,16 @@ def setup() -> None:
 
 # Draw loop
 def draw() -> None:
+    if agent.x >= W_WIDTH:
+        agent.x = 0
+    elif agent.x <= 0:
+        agent.x = W_WIDTH
+
+    if agent.y >= W_HEIGHT:
+        agent.y = 0
+    elif agent.y <= 0:
+        agent.y = W_HEIGHT
+
     agent.show()
     agent.step()
 
